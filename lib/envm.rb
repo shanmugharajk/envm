dcf042134284d52474dbfe88331e0e2c8f1adc5c
shanmugharajk <shanmugharaj.k@outlook.com>
2021-10-07 18:48:03 +0530

    feat: basic version to manage env

---
+++ lib/envm.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require_relative 'utils'
+
+INVALID_COMMAND_MSG = "\nYou've entered an invalid command. Please run the following to check all available commands
+$ envm help
+"
+
+LIST_OF_COMMANDS = "\nThe list of supported commands are => envm <lang> list, eg.
+
+envm list node                 -   Lists all the node versions installed
+envm global node <version>     -   Sets the <version> globally
+envm use node <version>        -   Sets the <version> locally
+"
+
+ENV_INVALID_MSG = "\nPlease set the env variables LANGS, ENVM_ROOT as follows
+$ export LANGS='node ruby'
+$ export ENVM_ROOT=<root path where envm the version folders are present>
+The folder structure should be like below
+
+  envm
+  | node
+  | |- versions
+  | | |- v10.24.1
+  |- ruby
+  | |- versions
+  | | |- 3.0.2
+"
+
+def handle_commands(argv)
+  lang = argv[1]
+  version = argv[2]
+
+  case argv.first
+  when 'help'
+    puts LIST_OF_COMMANDS
+  when 'init'
+    init
+  when 'list'
+    list(lang)
+  when 'global'
+    set_global_version(lang, version)
+  when 'local'
+    use_local_version(lang)
+  when 'use'
+    use_version(lang, version)
+  else
+    puts INVALID_COMMAND_MSG
+  end
+end
+
+def main
+  if !env_variables_valid?
+    puts ENV_INVALID_MSG
+  elsif ARGV.empty?
+    puts INVALID_COMMAND_MSG
+  else
+    handle_commands(ARGV)
+  end
+end
---
+++ lib/utils.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+LANGS = ENV['LANGS']
+ENVM_ROOT = ENV['ENVM_ROOT']
+
+# Helpers
+def blank?(string)
+  string.nil? || string.empty?
+end
+
+def env_variables_valid?
+  !(blank?(LANGS) || blank?(ENVM_ROOT))
+end
+
+def set_path(lang, version)
+  base_path = "#{ENV['ENVM_ROOT']}/#{lang}"
+  exe_path = "#{base_path}/versions/#{version}/bin"
+  envs = ENV['PATH'].split(':').reject { |env| env.include? base_path }
+
+  unless File.exist?(exe_path)
+    puts "The #{lang} - #{version} is not installed."
+    exit 1
+  end
+
+  ENV['PATH'] = "#{exe_path}:#{envs.uniq.join(':')}"
+end
+
+def init_shell_session
+  Kernel.exec(ENV['SHELL'])
+end
+
+def global_config_path(lang)
+  "#{ENV['ENVM_ROOT']}/.#{lang}_version"
+end
+
+def local_config_path(lang)
+  "#{Dir.pwd}/.#{lang}_version"
+end
+
+def read_global_config(lang)
+  filename = global_config_path(lang)
+  return File.read(filename).split[0] if File.exist?(filename)
+end
+
+def read_local_config(lang)
+  filename = local_config_path(lang)
+  return File.read(filename).split[0] if File.exist?(filename)
+end
+
+# Commands
+def init
+  LANGS.split.each do |lang|
+    config = read_local_config(lang) || read_global_config(lang)
+    set_path(lang, config)
+  end
+
+  init_shell_session
+end
+
+def list(lang)
+  if blank?(lang)
+    puts "Enter the language eg.\n$ envm list node"
+    return
+  end
+
+  root_path = "#{ENVM_ROOT}/#{lang}/versions"
+  versions = Dir.entries(root_path).reject { |file| File.directory?(file) || file == '.DS_Store' }
+  puts "\nThe available versions are \n#{versions.join("\n")} \n "
+end
+
+def set_global_version(lang, version)
+  if blank?(lang) || blank?(version)
+    puts "Please enter language, version eg.\n $ envm global node <version>."
+  else
+    File.write(global_config_path(version), version)
+    set_path(lang, version)
+    init_shell_session
+  end
+end
+
+def use_version(lang, version)
+  if blank?(lang) || blank?(version)
+    puts "Please enter language, version eg.\n $ envm local node <version>."
+  else
+    set_path(lang, version)
+    init_shell_session
+  end
+end
+
+def use_local_version(lang)
+  config = read_local_config(lang)
+
+  if blank?(config)
+    puts 'No local configuration files exists in the current directory.'
+    puts 'Create a file of format .<lang>_version and put the version in it.'
+  else
+    set_path(lang, config)
+    init_shell_session
+  end
+end
